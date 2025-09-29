3c407ca37e30018883524e31ac759b78d1116c88
Victor <49385026+Victor152M@users.noreply.github.com>
2025-09-29 12:06:35 +0300

    Update

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -12,20 +12,20 @@ const Contact = () => {
         <div className="mt-6 space-y-4">
             <p className="text-lg text-gray-100">
             📧 Email:  
-            <a href="mailto:victor.cristian.st@gmail.com" className="text-yellow-500 font-semibold hover:underline">
+            <a href="" className="text-yellow-500 font-semibold hover:underline">
                 email
             </a>
             </p>
             <p className="text-lg text-gray-100">
             🔗 LinkedIn:   
-            <a href="https://www.linkedin.com/in/victor-cristian-s-70577b239/" target="_blank" rel="noopener noreferrer"
+            <a href="" target="_blank" rel="noopener noreferrer"
                 className="text-yellow-500 font-semibold hover:underline">
                 linkedin
             </a>
             </p>
             <p className="text-lg text-gray-100">
             🔗 UpWork:   
-            <a href="https://www.upwork.com/freelancers/~01e1278ab489979deb/" target="_blank" rel="noopener noreferrer"
+            <a href="" target="_blank" rel="noopener noreferrer"
                 className="text-yellow-500 font-semibold hover:underline">
                 upwork
             </a>
